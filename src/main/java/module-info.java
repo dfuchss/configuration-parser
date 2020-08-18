63513c1a3886665ddfc9587d61dada572bc568a2
Dominik Fuchß <develop@fuchss.org>
2020-08-18 12:42:43 +0200

    Delete module-info.java

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module configparser {
-	exports org.fuchss.configuration;
-	exports org.fuchss.configuration.annotations;
-	exports org.fuchss.configuration.parser;
-	exports org.fuchss.configuration.setters;
-
-	requires org.apache.logging.log4j;
-}
